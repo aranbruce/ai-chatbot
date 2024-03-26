e44959cebb2350ff1bd59b1faa5314dbd53bf97a
Aran <34005105+aranbruce@users.noreply.github.com>
2024-03-26 22:44:00 +0000

    Fixed accidental theming

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -40,7 +40,7 @@ const EmptyScreen = ({handleExampleClick}:EmptyScreenProps) => {
         {exampleMessages.map((example, index) => (
           <div
             key={example.heading}
-            className={`cursor-pointer rounded-lg border bg-white p-4 hover:bg-zinc-50 dark:bg-zinc-950 dark:hover:bg-zinc-900 ${
+            className={`cursor-pointer rounded-lg border bg-white p-4 hover:bg-gray-100 ${
               index > 1 && 'hidden md:block'
             }`}
             onClick={() => handleExampleClick(example.message)}
