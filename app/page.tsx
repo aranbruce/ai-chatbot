740230d462a546e813f41696883a0f120c4c31fb
Aran Bruce-Caddick <aranbruce@gmail.com>
2024-06-06 23:51:39 +0100

    Increased timeout

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Chat from "@/components/chat";
 
 export const dynamic = "force-dynamic";
-export const maxDuration = 20;
+export const maxDuration = 30;
 
 export default function Page() {
   return <Chat />;
